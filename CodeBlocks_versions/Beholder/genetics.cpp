e7a085ef5f9e26259e6b20f38e8c7a1b984f1e44
unknown <markusjm@VW-VISUAL-05.pk.amk.root>
2014-05-21 12:00:03 +0300

    removed CB versions

--- CodeBlocks_versions/Beholder/genetics.cpp
+++
@@ -1,2 +0,0 @@
-#include "genetics.h"
-const double Neuron::ACTIVATION_RESPONSE = 1.0;
--- CodeBlocks_versions/Seekers/genetics.cpp
+++
@@ -1,5 +0,0 @@
-#include "genetics.h"
-const int GenParams::GENE_POOL_SIZE = 8;
-const double GenParams::PI = 3.1415926535897932384626433832795;
-const double GenParams::PI_2 = 1.5707963267948966192313216916398;
-const double GenParams::ACTIVATION_RESPONSE = 1.0;
